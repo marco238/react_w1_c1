881a1efd5e885735ec54ec837505eb8b6591411d
Marcos Monzón <marcos.monzon.iparraguirre.ext@ing.com>
2023-08-30 19:41:59 +0200

    add callbacks

--- src/components/greeting/Greeting.jsx
+++ src/components/greeting/Greeting.jsx
@@ -2,12 +2,13 @@ import './Greeting.css';
 
 /* eslint-disable react/prop-types */
 function Greeting(props) {
-  const { name, age } = props;
+  const { age, cb, name } = props;
 
   return (
     <div className="card">
       <h1>Hello, {name}! Hoy cumples {age} años</h1>
       <img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif" alt="birthday" />
+      <button type="button" onClick={() => cb(`Hola desde el componente hijo: ${name}`)}>Click me!</button>
     </div>
   )
 }
--- src/components/greetingList/GreetingList.jsx
+++ src/components/greetingList/GreetingList.jsx
@@ -6,12 +6,16 @@ function GreetingList() {
     { name: 'Jorge', age: 25 },
     { name: 'Luis', age: 30 },
     { name: 'María', age: 20 },
-  ];  
+  ]; 
+
+  const hendleClick = (word) => {
+    console.log(word);
+  }
 
   return (
     <div className="list">
       {users.map((user) => (
-        <Greeting name={user.name} age={user.age} key={user.name} />
+        <Greeting name={user.name} age={user.age} cb={hendleClick} key={user.name} />
       ))}
     </div>
   );
